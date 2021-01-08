8eca60b1faf044487fbe7dedfa058c516536669b
Steven Jack Chung <stevenjchung12@gmail.com>
2021-01-08 11:19:05 -0500

    Delete carousel in index.html and create a module for it

--- src/index.js
+++ src/index.js
@@ -2,36 +2,36 @@ import * as tabs from './tabs';
 import nav from './nav';
 import reset from './reset';
 import mainPage from './main_page';
+import carousel from './carousel';
 
 const content = document.querySelector('#content');
 
-document.body.appendChild(nav());
-document.body.appendChild(content);
+document.body.append(nav(), content, carousel());
 
 const home = document.querySelector('.home');
 const menu = document.querySelector('.menu');
 const contact = document.querySelector('.contact');
 
-const carousel = document.getElementById('carouselExampleCaptions');
+const carouselCont = document.getElementById('carouselExampleCaptions');
 
 content.append(...mainPage());
 
 home.addEventListener('click', () => {
-  const car = content.removeChild(carousel);
+  const car = content.removeChild(carouselCont);
   document.body.appendChild(car);
   reset();
-  carousel.style.display = 'none';
+  carouselCont.style.display = 'none';
   content.append(...mainPage());
 });
 
 menu.addEventListener('click', () => {
   reset();
-  carousel.style.display = 'block';
-  content.append(tabs.menu(), carousel);
+  carouselCont.style.display = 'block';
+  content.append(tabs.menu(), carouselCont);
 });
 
 contact.addEventListener('click', () => {
   reset();
-  carousel.style.display = 'none';
-  content.append(...tabs.contact(), carousel);
+  carouselCont.style.display = 'none';
+  content.append(...tabs.contact(), carouselCont);
 });
